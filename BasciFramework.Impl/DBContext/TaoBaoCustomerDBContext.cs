f415f570de2c06be6f7d1e23b28485cddf21c74f
qingsong.yang <qingsong.yang@chukou1.com>
2019-10-28 18:42:51 +0800

    升级到core3.0

---
+++ BasciFramework.Impl/DBContext/TaoBaoCustomerDBContext.cs
@@ -0,0 +1,30 @@
+﻿using BasciFramework.Dommain.Entitys.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasciFramework.Impl.DBContext
+{
+    public  class TaoBaoCustomerDBContext:DbContext
+    {
+        public TaoBaoCustomerDBContext(DbContextOptions<TaoBaoCustomerDBContext> dbContextOptions) :base(dbContextOptions)
+        {
+
+        }
+
+        /// <summary>
+        /// 用户基础信息
+        /// </summary>
+        public DbSet<UserEntity> UserEntitys { get; set; }
+        /// <summary>
+        /// 角色信息
+        /// </summary>
+        public DbSet<RoleEntity> RoleEntitys { get; set; }
+        /// <summary>
+        /// 用户角色
+        /// </summary>
+        public DbSet<UserRoleEntity> UserRoleEntitys { get; set; }
+        /// <summary>
+        /// 角色权限
+        /// </summary>
+        public DbSet<RoleRootEntity> RoleRootEntity { get; set; }
+    }
+}
