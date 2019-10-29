87ed07b440daac9bc93499a5d6671998393cb2fb
qingsong.yang <qingsong.yang@chukou1.com>
2019-10-29 18:39:24 +0800

    临时提交

---
+++ BasciFramework.Impl/Repositorys/BaseRepository.cs
@@ -0,0 +1,48 @@
+﻿using BasciFramework.Dommain.Entitys;
+using BasciFramework.Dommain.Repositorys;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasciFramework.Impl.Repositorys
+{
+    /// <summary>
+    /// 基础仓储实现
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        /// <summary>
+        /// 添加一个或者多个实体模型
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public async Task<bool> AddEntityAsync(params TEntity[] entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 修改一个或者多个实体模型
+        /// </summary>
+        /// <param name="entitys"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdateEntityAsync(params TEntity[] entitys)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 删除一个或者多个实体模型
+        /// </summary>
+        /// <param name="entitys"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteEntityAsync(params TEntity[] entitys)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
---
+++ BasciFramework.Impl/Repositorys/ReadOnlyBaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using BasciFramework.Dommain.Entitys;
+using BasciFramework.Dommain.Repositorys;
+using BasicFramework.Common.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasciFramework.Impl.Repositorys
+{
+
+    /// <summary>
+    /// 基础只读仓储实现
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public class ReadOnlyBaseRepository<TEntity> : IReadOnlyBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        public Task<int> CountAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByKeyAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<TEntity>> GetEntityAllListAsync(Expression<Func<TEntity, bool>> conditionExpression, Expression<Func<TEntity, dynamic>> groupbyExpression, bool isDesc = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetEntityAsync(Expression<Func<TEntity, bool>> conditionExpression, Expression<Func<TEntity, dynamic>> groupbyExpression = null, bool isDesc = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PageResult<TEntity>> GetEntityPageList(int pageIndex, int pageSize, Expression<Func<TEntity, bool>> conditionExpression = null, Expression<Func<TEntity, dynamic>> groupbyExpression = null, bool isDesc = true)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
