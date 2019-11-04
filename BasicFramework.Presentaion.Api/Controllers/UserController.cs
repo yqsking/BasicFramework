f940f45b3a3533db337105565fb1f58ba4f330bf
qingsong.yang <qingsong.yang@chukou1.com>
2019-11-04 14:14:11 +0800

    临时提交

--- BasicFramework.Presentaion.Api/Controllers/UserController.cs
+++ BasicFramework.Presentaion.Api/Controllers/UserController.cs
@@ -1,24 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net;
 using System.Threading.Tasks;
-using BasicFramework.Appliction.Commands.User;
 using BasicFramework.Appliction.Queries;
+using BasicFramework.Appliction.ViewModels.User;
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace BasicFramework.Presentaion.Api.Controllers
 {
     /// <summary>
     /// 用户模块
     /// </summary>
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        private readonly IUserQueries _userQueries;
         private readonly IMediator _mediator;
+        private readonly IUserQueries _userQueries;
+      
 
         /// <summary>
         /// 
@@ -30,30 +29,18 @@ namespace BasicFramework.Presentaion.Api.Controllers
             _mediator = mediator;
             _userQueries = userQueries;
         }
-        
+
         /// <summary>
-        /// 根据id获取用户基础信息
+        /// 获取指定用户信息
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
         [HttpGet]
         [Route("users/{id}")]
-        public async Task<IActionResult> GetUserById([FromRoute]string id)
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(UserResponseDto))]
+        public async Task<IActionResult> GetUserInfo([FromQuery]string id)
         {
            var result=await  _userQueries.GetUserById(id);
-            return Ok(result);
-        }
-
-        /// <summary>
-        /// 用户注册
-        /// </summary>
-        /// <param name="command"></param>
-        /// <returns></returns>
-        [HttpPost]
-        [Route("users/userRegister")]
-        public async Task<IActionResult> UserRegister([FromBody]UserRegisterCommand command)
-        {
-            var result = await _mediator.Send(command);
             return Ok(result);
         }
     }
