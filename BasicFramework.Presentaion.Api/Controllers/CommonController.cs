c721524c77b51fa8d722d9a9843fd150d83a8c68
qingsong.yang <qingsong.yang@chukou1.com>
2019-11-04 16:55:18 +0800

    临时提交

---
+++ BasicFramework.Presentaion.Api/Controllers/CommonController.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using BasicFramework.Appliction.Queries;
+using BasicFramework.Appliction.ViewModels.User;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BasicFramework.Presentaion.Api.Controllers
+{
+    /// <summary>
+    /// 公共模块
+    /// </summary>
+    [ApiController]
+    [Route("api/commons")]
+    public class CommonController : ControllerBase
+    {
+       
+        /// <summary>
+        /// 上传文件
+        /// </summary>
+        /// <param name="file">上传文件</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("upload")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(bool))]
+        public async Task<IActionResult> GetUserInfo([FromBody]IFormFile file)
+        {
+           return Ok(await Task.FromResult(true));
+          
+        }
+    }
+}
